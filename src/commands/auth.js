212d4a6640d0bc2580b6adcb47825be06b71ac50
Eugene Obrezkov <ghaiklor@gmail.com>
2016-04-19 21:37:32 +0300

    fix(command): Fix issue with no sending messages in auth

--- src/commands/auth.js
+++ src/commands/auth.js
@@ -13,21 +13,21 @@ module.exports = bot => {
     if (!username || !token) return bot.sendMessage(telegramId, MESSAGES.USERNAME_AND_TOKEN_NOT_SPECIFIED);
 
     User.findOne({username, telegramId}, (error, user) => {
-      if (error) return bot.sendMessage(user.telegramId, MESSAGES.SOMETHING_WENT_WRONG);
+      if (error) return bot.sendMessage(telegramId, MESSAGES.SOMETHING_WENT_WRONG);
 
       if (!user) {
         User.create({username, token, telegramId}, (error, user) => {
-          if (error) return bot.sendMessage(user.telegramId, MESSAGES.SOMETHING_WENT_WRONG);
+          if (error) return bot.sendMessage(telegramId, MESSAGES.SOMETHING_WENT_WRONG);
 
           bot.sendMessage(telegramId, MESSAGES.REGISTER_SUCCESSFUL);
-          new GitHubNotifications(user.username, user.token).on('notification', data => bot.sendMessage(user.telegramId, data));
+          new GitHubNotifications(user.username, user.token).on('notification', data => bot.sendMessage(telegramId, data));
         });
       } else {
         User.update({username, telegramId}, {token}, (error, user) => {
           if (error) return bot.sendMessage(telegramId, MESSAGES.SOMETHING_WENT_WRONG);
 
-          bot.sendMessage(user.telegramId, MESSAGES.PERSONAL_TOKEN_UPDATED);
-          new GitHubNotifications(user.username, user.token).on('notification', data => bot.sendMessage(user.telegramId, data));
+          bot.sendMessage(telegramId, MESSAGES.PERSONAL_TOKEN_UPDATED);
+          new GitHubNotifications(user.username, user.token).on('notification', data => bot.sendMessage(telegramId, data));
         });
       }
     });
