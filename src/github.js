c71d67115ab3bfa9056252ee0c6a184eec425b3f
Eugene Obrezkov <ghaiklor@gmail.com>
2016-04-19 18:27:32 +0300

    Getting ready for first deploy

--- src/github.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const GitHubApi = require('github');
-const github = new GitHubApi({version: '3.0.0'});
-
-module.exports = github;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 "use strict";
 
-const telegram = require('./telegram');
-const github = require('./github');
+const requireAll = require('require-all');
+const TelegramBot = require('node-telegram-bot-api');
+
+const BOT_COMMANDS = requireAll({dirname: `${__dirname}/commands`});
+const TELEGRAM_BOT_TOKEN = process.env['TELEGRAM_BOT_TOKEN'];
+
+if (!TELEGRAM_BOT_TOKEN) throw new Error('You must provide telegram bot token');
+
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, {polling: true});
+
+Object.keys(BOT_COMMANDS).forEach(command => BOT_COMMANDS[command](bot));
