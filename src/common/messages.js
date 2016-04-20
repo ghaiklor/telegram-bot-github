046fa9ac002c82caa88b5eae7fd1c876dc78da6d
Eugene Obrezkov <ghaiklor@gmail.com>
2016-04-20 11:00:08 +0300

    feat(messages): Add few more messages

--- src/common/messages.js
+++ src/common/messages.js
@@ -1,7 +1,9 @@
 module.exports = {
   SOMETHING_WENT_WRONG: `Something went wrong, try again...`,
-  USERNAME_AND_TOKEN_NOT_SPECIFIED: `You should specify username and token!`,
-  REGISTER_SUCCESSFUL: `You successfully has been registered`,
-  PERSONAL_TOKEN_UPDATED: `Your personal token successfully has been updated`,
-  USER_NOT_EXISTS: `I can't find GitHub profile`
+  USERNAME_NOT_SPECIFIED: `You should specify username. Try to authorize via /auth command again.`,
+  GITHUB_TOKEN_NOT_SPECIFIED: `You should specify your GitHub personal token. Try to authorize via /auth command again.`,
+  REGISTER_SUCCESSFUL: `You have successfully been registered. When you get new notifications, I will send you a message.`,
+  PERSONAL_TOKEN_UPDATED: `Your personal token has successfully been updated.`,
+  USER_NOT_EXISTS: `I can't find GitHub profile with this username. You can register your GitHub account via /auth command.`,
+  CANT_UPDATE_PERSONAL_TOKEN: `I can't update personal token for this user. Maybe it's not your GitHub username?`
 };
