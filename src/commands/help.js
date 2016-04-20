705201e857c9c0b61ec5a2d79ba8b522257f2a11
Eugene Obrezkov <ghaiklor@gmail.com>
2016-04-20 11:21:02 +0300

    fix(command): Fix issue with help styling

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,14 +5,14 @@ module.exports = bot => {
     const fromId = message.from.id;
 
     bot.sendMessage(fromId, `
-    Hi!
+Hi!
 
-    I'm here to notify you when something interesting happens on your GitHub profile.
-    Although, you will need to tell me your username and personal token for me to do that.
+I'm here to notify you when something interesting happens on your GitHub profile. Although, you will need to tell me your username and personal token for me to do that.
 
-    If you don't know how to generate your personal token, follow this link - https://help.github.com/articles/creating-an-access-token-for-command-line-use/.
-    Your personal token must have 'repo' and 'notifications' scope, otherwise, I can't guarantee that I can notify you.
-    After you generate your personal token, call me via /auth username:token command, thanks.
+If you don't know how to generate your personal token, follow this link - https://help.github.com/articles/creating-an-access-token-for-command-line-use/.
+
+Your personal token must have 'repo' and 'notifications' scope, otherwise, I can't guarantee that I can notify you.
+After you generate your personal token, call me via /auth username:token command, thanks.
     `);
   });
 };
