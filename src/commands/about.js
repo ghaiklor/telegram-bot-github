a0e6fe9493a320d6d3cab513947bb8fb915aba7d
Eugene Obrezkov <ghaiklor@gmail.com>
2016-04-21 12:15:07 +0300

    fix(messages): Typo fixes

--- src/commands/about.js
+++ src/commands/about.js
@@ -9,8 +9,8 @@ Developer - Eugene Obrezkov aka ghaiklor.
 Follow me on Twitter - https://twitter.com/ghaiklor.
 Follow me on GitHub - https://github.com/ghaiklor.
 
-You can report issues directly to me in Twitter and I'll answer as soon as possible, thanks.
-    `;
+You can report issues directly to me via Twitter and I'll respond as soon as possible, thanks.
+`;
 
     bot.sendMessage(fromId, response);
   });
